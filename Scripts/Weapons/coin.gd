7118249da62f6644e04d85f95d8cf5b5a52bca1e
Hamza Latif <hamzalatif@hotmail.co.uk>
2026-06-07 00:27:05 +0100

    Created coin scene with placeholder graphics and dummy functions.

---
+++ Scripts/Weapons/coin.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _physics_process(delta: float) -> void:
+	# Fly forward
+	# in an arc? up and over?
+	pass
+	
+func hit(killer, weapon, damage, knockback, instakill):
+	# Called when bullet hits this
+	# Kertwang into nearest enemy
+	pass
