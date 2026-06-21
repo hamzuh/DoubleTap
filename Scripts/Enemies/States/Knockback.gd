f177bdad5f5fd2f6feff6ce56cf12fc5fc8928e2
Hamza Latif <hamzalatif@hotmail.co.uk>
2026-06-21 16:51:47 +0100

    Added animations and sound effects for the zombie enemy on hit, stun and swipe.

--- Scripts/Enemies/States/Knockback.gd
+++ Scripts/Enemies/States/Knockback.gd
@@ -1,13 +1,18 @@
 extends State
 
 @export var idle_state: State
+var length: float
 
 func enter() -> void:
+	length = parent.knockforce / 1000
+	print(length)
+	animations.sprite_frames.set_animation_speed("knocked_back", 6 / length)
 	super()
 	$"../../Hitboxes/Knockback".set_deferred("disabled", false)
 
 # Animation doesn't loop and the length of knockback is set
 # This means we exit through this instead of the animation ended signal
+# Speed up animation through code to fit
 func process_physics(delta: float) -> State:
 	parent.velocity = parent.knockdirect * parent.knockforce * Globals.speed_scale
 	parent.move_and_slide()
--- Scripts/Enemies/States/Swiping.gd
+++ Scripts/Enemies/States/Swiping.gd
@@ -3,7 +3,16 @@ extends State
 @export var idle_state: State
 @export var chasing_state: State
 
+@onready var sfxPlayer = $"../../AudioStreamPlayer2D"
+@export var sfxArray: Array[AudioStream]
+
 var finished: bool = false
+
+func enter() -> void:
+	super()
+	# Start cooldown timer for use in other states
+	sfxPlayer.stream = sfxArray.pick_random()
+	sfxPlayer.play()
 
 # This'll require the animation player instead to animate hitboxes
 func _on_animated_sprite_2d_animation_finished() -> void:
@@ -12,8 +21,13 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 			
 func process_physics(delta: float) -> State:
 	if finished:
+		finished = false
 		if parent.position.distance_to(parent.Player.position) >= 100:
 			return chasing_state
 		else:
 			return idle_state
 	return null
+
+func exit() -> void:
+	if sfxPlayer.playing:
+		sfxPlayer.stop()
